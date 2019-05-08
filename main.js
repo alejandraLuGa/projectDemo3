3b5d8a14976f8ae92a2bcd12d473df44a7b478fa
jennifer19971127 <47649306+jennifer19971127@users.noreply.github.com>
2019-05-08 00:50:19 -0400

    test

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const {app, BrowserWindow} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
