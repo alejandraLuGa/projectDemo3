f3b418e559b9469f123bcdc459fc059bcc5d25a0
alejandraLuGa <alunagas@buffalo.edu>
2019-05-09 11:28:41 -0400

    xdsdsds

--- Getid.js
+++
@@ -1,9 +0,0 @@
-function getid(e){
-    var set = document.getElementById("set");
-    var k = document.createElement("P");
-    k.setAttribute("type", "text");
-    k.setAttribute("id", "text");
-    set.appendChild(k);
-    document.getElementById("text").value = e;
-
-}
